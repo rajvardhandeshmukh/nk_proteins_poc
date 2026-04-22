937030dffac85d15ef1f9c144347ee85330a4f29
Rajvardhan Deshmukh~ <rajvardhan.deshmukh@algorisys.com>
2026-04-22 10:11:13 +0530

    feat: setup isolated v2 templates file for new query logic

---
+++ gateway/sql_templates_v2.py
@@ -0,0 +1,11 @@
+"""
+V2 SQL Templates for NK Protein POC.
+This file contains pure query-to-data extraction logic, 
+strictly separated from previous iterations to avoid interference.
+"""
+
+SQL_TEMPLATES = {
+    # The user will provide the query logic to be placed here.
+}
+
+VALID_INTENTS = list(SQL_TEMPLATES.keys())
